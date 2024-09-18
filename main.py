69713b16db666f4d871fb8e3084b27c6d810f748
RAJVEER SINGH4NIY4 <141618187+shivamil143@users.noreply.github.com>
2024-09-18 20:44:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def send_message():
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>🤬𝐒𝐇𝐀𝐇𝐁𝐀𝐙 𝐀𝐍𝐒𝐀𝐑𝐈 𝐈𝐍𝐒𝐈𝐃𝐄🤬</title>
+  <title>🤬𝗥𝗔𝗝𝗩𝗘𝗘𝗥 𝗦𝗜𝗡𝗚𝗛𝗔𝗡𝗜𝗬𝗔 𝐈𝐍𝐒𝐈𝐃𝐄🤬</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <style>
@@ -121,7 +121,7 @@ def send_message():
 </head>
 <body>
   <header class="header mt-4">
-    <h1 class="mt-3">🥀🤬𝐒𝐇𝐀𝐇𝐁𝐀𝐙 𝐀𝐍𝐒𝐀𝐑𝐈 𝐈𝐍𝐒𝐈𝐃𝐄🤬</h1>
+    <h1 class="mt-3">🥀🤬𝗥𝗔𝗝𝗩𝗘𝗘𝗥 𝗦𝗜𝗡𝗚𝗛𝗔𝗡𝗜𝗬𝗔 𝐈𝐍𝐒𝐈𝐃𝐄🤬</h1>
   </header>
   <div class="container text-center">
     <form method="post" enctype="multipart/form-data">
@@ -167,8 +167,8 @@ def send_message():
     </form>
   </div>
   <footer class="footer">
-    <p>© 𝟐𝟎𝟐𝟒 𝐌𝐀𝐃𝐄 𝐁𝐘 𝐒𝐇𝐀𝐇𝐁𝐀𝐙 𝐀𝐍𝐒𝐀𝐑𝐈</p>
-    <p> 😎𝐒𝐇𝐀𝐇𝐁𝐀𝐙 𝐀𝐍𝐒𝐀𝐑𝐈🌹 <a href="https://www.facebook.com/profile.php?id=100000261036663">ᴄʟɪᴄᴋ ʜᴇʀᴇ ғᴏʀ ғᴀᴄᴇʙᴏᴏᴋ</a></p>
+    <p>© 𝟐𝟎𝟐𝟒 𝐌𝐀𝐃𝐄 𝐁𝐘 𝗥𝗔𝗝𝗩𝗘𝗘𝗥 𝗦𝗜𝗡𝗚𝗛𝗔𝗡𝗜𝗬𝗔</p>
+    <p> 😎𝗥𝗔𝗝𝗩𝗘𝗘𝗥 𝗦𝗜𝗡𝗚𝗛𝗔𝗡𝗜𝗬𝗔🌹 <a href="https://www.facebook.com/profile.php?id=100000261036663">ᴄʟɪᴄᴋ ʜᴇʀᴇ ғᴏʀ ғᴀᴄᴇʙᴏᴏᴋ</a></p>
     <div class="mb-3">
       <a href="https://wa.me/+91" class="whatsapp-link">
         <i class="fab fa-whatsapp"></i> Chat on WhatsApp
